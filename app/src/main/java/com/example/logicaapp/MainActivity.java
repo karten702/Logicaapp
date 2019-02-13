e8d8ae21d610437cf46afd6259586743244ff0e8
Martijn <karten702@gmail.com>
2019-02-13 10:54:13 +0100

    Fixed edittext bug. now gives the correct result

--- app/src/main/java/com/example/logicaapp/MainActivity.java
+++ app/src/main/java/com/example/logicaapp/MainActivity.java
@@ -1,31 +1,29 @@
 package com.example.logicaapp;
 
-import android.support.design.widget.TextInputEditText;
-import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextInputLayout answer1;
-    private TextInputLayout answer2;
-    private TextInputLayout answer3;
-    private TextInputLayout answer4;
+    private EditText answer1;
+    private EditText answer2;
+    private EditText answer3;
+    private EditText answer4;
     private Button submitButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        answer1 = findViewById(R.id.textInputLayout);
-        answer2 = findViewById(R.id.textInputLayout2);
-        answer3 = findViewById(R.id.textInputLayout3);
-        answer4 = findViewById(R.id.textInputLayout4);
+        answer1 = findViewById(R.id.Editor1);
+        answer2 = findViewById(R.id.editor2);
+        answer3 = findViewById(R.id.editor3);
+        answer4 = findViewById(R.id.editor4);
         submitButton = findViewById(R.id.submitButton);
 
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -41,17 +39,18 @@ public class MainActivity extends AppCompatActivity {
         String answerString2 = GetAnswerFromInput(answer2);
         String answerString3 = GetAnswerFromInput(answer3);
         String answerString4 = GetAnswerFromInput(answer4);
-        if (answerString1.toLowerCase() == "t" && answerString2.toLowerCase() == "f" && answerString3.toLowerCase() == "f" && answerString4.toLowerCase() == "t"){
+        if (answerString1.equalsIgnoreCase("t") && answerString2.equalsIgnoreCase("f") && answerString3.equalsIgnoreCase("f") && answerString4.equalsIgnoreCase("t")){
             Toast.makeText(this, getString(R.string.answerCorrect), Toast.LENGTH_SHORT).show();
         }
         else
             Toast.makeText(this, getString(R.string.answerIncorrect), Toast.LENGTH_SHORT).show();
     }
 
-    private String GetAnswerFromInput(TextInputLayout editor){
+    private String GetAnswerFromInput(EditText editor){
         String value = "";
-        if(editor.getEditText().getText().length() > 1){
-            value = editor.getEditText().getText().toString();
+
+        if(editor.getEditableText().length() > 0){
+            value = editor.getEditableText().toString();
         }
         return value;
     }
